a28ad98cc0394094a0bddea02312e578642147fe
eugeniopoppi <eugeniopoppi@gmail.com>
2025-09-03 10:24:25 +0200

    Update README.c

--- README.c
+++ README.c
@@ -1,5 +1,9 @@
+////////////some codes for the exam "Numerical Methods" University of Physics ////////////
+///////////////////////////////////////////////////////////////////////////////////////
+
 //2dU1 is a two dimensional U(1) gauge theory- still working in progress
 //free_scalar_th is a two dimensional quantum scalar field, provide a computation of energy dentisy
+//ising2d_metro is a two dimensional ising model with metropolis update
 //ising_cluster is an ising model with cluster update
-//xy_auto is a O(2) model with micro and metro updates
+//xy_auto is a O(2) model with microcanonical and metropoli updates
 //the header is necessary in all the codes
